d3b4d5483414339cbe3f642c0ad9b0d7e0eedcef
Ganoma86 <144219639+Ganoma86@users.noreply.github.com>
2024-04-20 11:28:59 +0200

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Ganoma86.py
@@ -0,0 +1,34 @@
+# Esto es la web oficial de Python: https://www.python.org/
+
+# Esto es un comentario de una línea.
+
+"""
+esto es
+un comentario
+de varias
+lineas
+"""
+
+# En este apartado vamos a declarar una variable.
+name = "Gabriel"
+
+# Variable con datos de texto
+lenguaje = "Python"
+
+# Variable con datos numéricos.
+edad = 37
+
+# Float
+estatura = 1.71
+
+# Variable con dato booleanos.
+estado = True
+
+# En python no se haceptan constantes por lo que se representan como variables.
+
+print("Hola, mi nombre es " + name + ", y esto está escrito en " + lenguaje)
+
+print(type(name))
+print(type(edad))
+print(type(estado))
+print(type(estatura))
